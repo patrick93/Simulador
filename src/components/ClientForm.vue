9b5024e604df22d3fe050c88551110d02d8d6968
Patrick Gazaneo <pck1993@gmail.com>
2018-02-08 18:51:35 -0200

    CNPJ and email fields

--- src/components/ClientForm.vue
+++ src/components/ClientForm.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="client-form">
-    <h4>Client</h4>
+    <form>
+      <div class="form-group">
+        <label class="form-control-label">CNPJ</label>
+        <input type="text" v-model="cnpj" class="form-control">
+      </div>
+      <div class="form-group">
+        <label class="form-control-label">Email</label>
+        <input type="email" v-model="email" class="form-control">
+      </div>
+    </form>
+    <button class="btn btn-success submit-button" v-on:click="submit">Enviar</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ClientForm'
+  name: 'ClientForm',
+  data () {
+    return {
+      email: '',
+      cnpj: ''
+    }
+  },
+  methods: {
+    submit: function () {
+      console.log(this.email)
+      console.log(this.cnpj)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.submit-button {
+  float: right;
+}
 </style>
